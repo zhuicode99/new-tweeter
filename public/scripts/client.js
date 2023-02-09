cb2a932a05bed16325d12c4abc2e014e66089ceb
zhuicode99 <enlin291814833@hotmail.com>
2023-02-09 17:35:21 -0500

    add new-tweet text area

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,10 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+/* const tex = document.getElementsByClassName('tex'); */
+/* const xx = document.getElementsByClassName('nav-right');
+nav-right.addEventListener("click", () => {
+  alert("You just clicked somewhere on this page.");
+});
+
+ */
